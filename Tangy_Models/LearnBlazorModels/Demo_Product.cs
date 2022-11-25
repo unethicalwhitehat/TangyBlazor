122fb51a3f022df39ff4413a73f174bc4ba0def4
unethicalwhitehat <61070079+unethicalwhitehat@users.noreply.github.com>
2022-11-25 16:19:18 +0000

    Progressed to latest point in course

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels
+{
+	public class Demo_Product
+	{
+
+		public int ID { get; set; }
+
+		public string Name { get; set; }
+
+		public double Price { get; set; }
+
+		public bool IsActive { get; set; }
+
+		public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+	}
+}
+
---
+++ Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels
+{
+	public class Demo_ProductProp
+	{
+		public int ID { get; set; }
+
+		public string Key { get; set; }
+
+		public string Value { get; set; }
+	}
+}
+

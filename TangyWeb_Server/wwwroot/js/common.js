b2997f95e552d64d80e4914e514dd347784b6d84
unethicalwhitehat <61070079+unethicalwhitehat@users.noreply.github.com>
2022-12-05 01:18:22 +0000

    Lec. 50

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -1,8 +1,8 @@
-﻿//window.SHowToastr = (type, message) => {
-//    if (type == "success") {
-//        toastr.success(message, "Operation Successful", { timeOut: 5000 });
-//    }
-//    if (type == "error") {
-//        toastr.success(message, "Operation Failed", { timeOut: 5000 });
-//    }
-//}
+﻿window.ShowToastr = (type, message) => {
+    if (type == "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type == "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}

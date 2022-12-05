91c976f38364f99944f2df67545390aeb09fbc84
unethicalwhitehat <61070079+unethicalwhitehat@users.noreply.github.com>
2022-12-05 00:59:41 +0000

    Broken?

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿//window.SHowToastr = (type, message) => {
+//    if (type == "success") {
+//        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+//    }
+//    if (type == "error") {
+//        toastr.success(message, "Operation Failed", { timeOut: 5000 });
+//    }
+//}

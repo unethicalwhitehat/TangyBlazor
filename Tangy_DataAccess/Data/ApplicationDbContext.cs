c032a3a430ad2338d939c0b2911af580d5b5203e
Justin Tyson <61070079+unethicalwhitehat@users.noreply.github.com>
2022-12-09 16:03:01 +0000

    Lec 60?

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace Tangy_DataAccess.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContentOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
